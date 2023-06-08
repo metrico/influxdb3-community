ae2e13894225a185cd22b4a6d54f344b58a932b6
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-06-08 14:05:53 +0200

    Update and rename examples/go/influxdb3.go to examples/go/influxdb3-go/client.go
    Signed-off-by: Lorenzo Mangani <lorenzo.mangani@gmail.com>

--- examples/go/influxdb3-go/client.go
+++ examples/go/influxdb3-go/client.go
@@ -6,16 +6,28 @@ import (
 	"flag"
 	"fmt"
 	"github.com/InfluxCommunity/influxdb3-go/influx"
+	"os"
 )
 
 func main() {
 
-	query := flag.String("query", "SELECT 1", "FlightSQL Query")
+	// Get Query settings
+	query := flag.String("query", "SELECT 'flight' as hello", "FlightSQL Query")
 	database := flag.String("database", "company_sensors", "FlightSQL Bucket")
 	url := flag.String("url", "http://localhost:8082", "FlightSQL API URL")
 	token := flag.String("token", "", "FlightSQL Auth Token")
+	if os.Getenv("INFLUXDB_URL") != "" {
+		flag.Set("url", os.Getenv("INFLUXDB_URL"))
+	}
+	if os.Getenv("INFLUXDB_TOKEN") != "" {
+		flag.Set("token", os.Getenv("INFLUXDB_TOKEN"))
+	}
+	if os.Getenv("INFLUXDB_DATABASE") != "" {
+		flag.Set("database", os.Getenv("INFLUXDB_DATABASE"))
+	}
 	flag.Parse()
 
+	// Initialize IOx Client
 	client, err := influx.New(influx.Configs{
 		HostURL:   *url,
 		AuthToken: *token,
@@ -31,12 +43,15 @@ func main() {
 		}
 	}(client)
 
+
+	// Execute Query
 	iterator, err := client.Query(context.Background(), *database, *query)
 
 	if err != nil {
 		panic(err)
 	}
 
+	// Return JSON
 	results := []map[string]interface{}{}
 
 	for iterator.Next() {
